40ce894564d70ea6f3d86dfca1238e6dd7a20100
Millan Uka <ukamillan@gmail.com>
2024-08-11 17:49:53 +1200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to product-catalog-app
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/product_catalog_app/core_test.clj
@@ -0,0 +1,7 @@
+(ns product-catalog-app.core-test
+  (:require [clojure.test :refer :all]
+            [product-catalog-app.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
