384acb0946ded16dd73e1f9adb196332db6255cd
Millan Uka <ukamillan@gmail.com>
2024-08-12 21:51:17 +1200

    Small changes

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/java.jdbc "0.7.12"]
                  [org.xerial/sqlite-jdbc "3.46.0.1"]
                  [ring/ring-core "1.12.2"]
-                 [ring/ring-jetty-adapter "1.12.2"]]
+                 [ring/ring-jetty-adapter "1.12.2"]
+                 [clj-http "3.13.0"]]
   :main ^:skip-aot product-catalog-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
