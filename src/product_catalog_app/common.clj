0b4a91439cbe53773eeb4ede5ae7e24e9fc4cda7
Millan Uka <ukamillan@gmail.com>
2024-08-13 21:54:40 +1200

    Add stripe http call

---
+++ src/product_catalog_app/common.clj
@@ -0,0 +1,5 @@
+(ns product-catalog-app.common)
+
+(defonce stripe-base-url "https://api.stripe.com")
+
+
--- src/product_catalog_app/stripe.clj
+++ src/product_catalog_app/stripe.clj
@@ -1,1 +1,9 @@
-(ns product-catalog-app.stripe)
+(ns product-catalog-app.stripe
+  (:require [clj-http.client :as client]
+            [product-catalog-app.common :as common])
+)
+
+
+(defn create-stripe-session
+(client/post (str stripe-base-url "v1/checkout/sessions") {:basic-auth "sk_token"
+                                                           :body})
